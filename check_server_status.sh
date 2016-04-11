c3d5320bbb68345b206b792bc45daafe1e03e373
unmattavesa <unmattavesa@gmail.com>
2016-04-11 22:25:12 +0530

    added output for quiet mode of ping

--- check_server_status.sh
+++ check_server_status.sh
@@ -12,3 +12,23 @@ fi
 rm -Rf temp.t
 
 
+# Output WITH -q Option
+
+# PING localhost (127.0.0.1): 56 data bytes
+
+# --- localhost ping statistics ---
+# 3 packets transmitted, 3 packets received, 0.0% packet loss
+# round-trip min/avg/max/stddev = 0.070/0.077/0.087/0.007 ms 
+
+
+
+# Output WITHOUT -q Option
+
+# PING localhost (127.0.0.1): 56 data bytes
+# 64 bytes from 127.0.0.1: icmp_seq=0 ttl=64 time=0.067 ms
+# 64 bytes from 127.0.0.1: icmp_seq=1 ttl=64 time=0.068 ms
+# 64 bytes from 127.0.0.1: icmp_seq=2 ttl=64 time=0.074 ms
+
+# --- localhost ping statistics ---
+# 3 packets transmitted, 3 packets received, 0.0% packet loss
+# round-trip min/avg/max/stddev = 0.067/0.070/0.074/0.003 ms
